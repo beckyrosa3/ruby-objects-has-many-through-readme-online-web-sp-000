a7a2dffc58c5de886ccbec118d86ec065f03d386
Becky Rosa <becky_m_rosa@yahoo.com>
2021-05-16 17:13:56 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,10 +14,6 @@ class Customer
     @@all
   end
 
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
-
   def meals
     Meal.all.select do |meal|
       meal.customer == self
@@ -29,4 +25,9 @@ class Customer
         meal.waiter
     end
   end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
 end
